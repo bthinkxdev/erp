17254110beef52588af0a4bd07fd6cf58e759789
bthinkxdev <bthinkx@gmail.com>
2026-04-02 21:46:33 +0530

    docker updated

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,4 +4,7 @@ set -e
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-exec gunicorn --bind 0.0.0.0:8000 --workers 3 erp.wsgi:application
+if [ "$#" -eq 0 ]; then
+  set -- gunicorn erp.wsgi:application --bind 0.0.0.0:8000
+fi
+exec "$@"
